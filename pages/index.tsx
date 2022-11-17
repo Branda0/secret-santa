4e840a01055ee0079592ef1590482d90c4445711
Branda0 <g.brandao.dev@gmail.com>
2022-11-17 21:54:55 +0100

    building home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
-import { useCallback, useEffect, useLayoutEffect, useMemo, useState } from "react";
-// import styles from "../styles/Home.module.css";
+import { Group } from "../types/types";
 import { getAllGroups } from "../lib/groups";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import Modal from "../components/Modal";
-import { Button } from "../components/Buttons";
-
-import clientPromise from "../lib/mongodb";
-
-type Group = {
-  name: string;
-  _id: number;
-};
+import Layout from "../components/Layout";
 
 export default function Home({ groups }: { groups: Array<Group> }) {
   const [isLoading, setLoading] = useState(false);
@@ -27,35 +22,44 @@ export default function Home({ groups }: { groups: Array<Group> }) {
   console.log("render Home");
 
   return (
-    <div>
+    <>
       <Head>
         <title>Secret Santa</title>
         <meta name="description" content="secret santa home page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <section>
-          {groups.map((group) => (
-            <Link key={group._id} href={`/${group.name}`}>
-              <h1 className="text-start text">{group.name}</h1>
-            </Link>
-          ))}
-        </section>
-        <Button handleClick={handleAddModal}>Ajouter un groupe</Button>
-      </main>
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+
+      <Layout>
+        <div className="flex flex-col ">
+          <div className=" p-4 text-gray-800 ">
+            <h2 className=" font-semibold text-lg mb-2  ">Comment ça marche ?</h2>
+            <p className="text-justify ">
+              Tu cherches à organiser un échange de cadeaux avec tes amis ou de la famille ? Commence donc par
+              créer ton groupe, puis invite chaque participant à visiter cette page, chacun pourra alors se
+              connecter à son propre espace et connaitre l'identité de son "secret santa"
+            </p>
+          </div>
+          <button
+            className=" self-center flex justify-center items-center w-1/2 btn-red"
+            onClick={handleAddModal}
+          >
+            <FontAwesomeIcon icon={faPlus} className="w-4 text-white mr-2" />
+            Ajouter un groupe
+          </button>
+          <div className=" p-4 text-gray-800 ">
+            <h2 className=" font-semibold text-lg mb-2  ">Rejoind ton groupe !</h2>
+            <section>
+              {groups.map((group) => (
+                <Link key={group._id} href={`/${group.name}`}>
+                  <h1 className="text-start text">{group.name}</h1>
+                </Link>
+              ))}
+            </section>
+          </div>
+        </div>
+      </Layout>
       {showModal && <Modal onClose={() => setShowModal(false)} />}
-    </div>
+    </>
   );
 }
 
