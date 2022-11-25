fb36cbcbfd7823bd3a16f44e858ee7fd489e9b4f
Branda0 <g.brandao.dev@gmail.com>
2022-11-25 17:19:04 +0100

    updated api/groups added graph compute logic

--- pages/api/groups/index.ts
+++ pages/api/groups/index.ts
@@ -1,6 +1,7 @@
 import clientPromise from "../../../lib/mongodb";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getAllGroups } from "../../../lib/groups";
+import { computeGraph } from "../../../lib/compute";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
@@ -9,16 +10,37 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         const client = await clientPromise;
         const db = client.db("secret_santa");
         const { name, members } = req.body;
+        console.log("okoko", members);
+        const membersSanta = computeGraph(members);
+
+        // if graph compute fails => too much constrains
+        if (!membersSanta) {
+          res.status(400).json({
+            error: { message: "Distribution des cadeaux impossible ! Modifiez vos contraintes de groupe" },
+          });
+        }
+
         if (name && members) {
           if (await db.collection("groups").findOne({ name: name })) {
             res.status(409).json({ error: { message: "Nom de groupe déjà existant !" } });
           }
 
           const membersResponse = await db.collection("members").insertMany(members);
+          const membersNumber = membersSanta.length;
+
+          for (let i = 0; i < membersNumber - 1; i++) {
+            await db
+              .collection("members")
+              .updateOne({ name: membersSanta[i] }, { $set: { secret: membersSanta[i + 1] } });
+          }
+          await db
+            .collection("members")
+            .updateOne({ name: membersSanta[membersNumber - 1] }, { $set: { secret: membersSanta[0] } });
+
           const groupResponse = await db.collection("groups").insertOne({ name, members });
 
           res.status(200).json({
-            data: await db.collection("groups").findOne({ _id: groupResponse.insertedId }),
+            // data: await db.collection("groups").findOne({ _id: groupResponse.insertedId }),
           });
           break;
         } else {
@@ -31,6 +53,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         break;
     }
   } catch (e) {
-    res.status(400).json({ error: { message: "Erreur de création de groupe, veuillez réessayer" } });
+    console.log(e);
+    res.status(400).json({ error: { message: "Error" } });
   }
 };
