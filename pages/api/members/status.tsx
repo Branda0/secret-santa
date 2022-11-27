7d92f846c570cf3e23ccf6d2f9bc8537206e0eb6
Branda0 <g.brandao.dev@gmail.com>
2022-11-27 22:38:46 +0100

    added api/members/status route / get status of member, ( ie :  has added a password for his account)

---
+++ pages/api/members/status.tsx
@@ -0,0 +1,34 @@
+import clientPromise from "../../../lib/mongodb";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { ObjectId } from "mongodb";
+import Member from "../../[groupId]/[memberId]";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { id } = req.query; // dynamic id for group specific endpoint
+    const client = await clientPromise;
+    const db = client.db("secret_santa");
+
+    if (!id) return res.status(400).json({ message: "Missing body parameters" });
+
+    switch (req.method) {
+      case "GET":
+        const user = await db.collection("members").findOne({ _id: new ObjectId(id) });
+        if (!user) return res.status(400).json({ message: "User not found" });
+
+        if (user.account) {
+          return res
+            .status(200)
+            .json({ member: { _id: user._id, name: user.name, group: user.group }, status: "signed" });
+        } else {
+          return res.status(200).json({
+            member: { _id: user._id, name: user.name, group: user.group },
+            status: "awaiting signup",
+          });
+        }
+    }
+  } catch (e) {
+    res.status(400).json({ error: { message: "Error" } });
+  }
+};
