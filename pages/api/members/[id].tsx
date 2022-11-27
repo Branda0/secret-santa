a06280e8c572f68217822dea50a817cd72fc450e
Branda0 <g.brandao.dev@gmail.com>
2022-11-27 22:43:46 +0100

    auth route for getting secret santa data from member selection panel

---
+++ pages/api/members/[id].tsx
@@ -0,0 +1,43 @@
+import clientPromise from "../../../lib/mongodb";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { ObjectId } from "mongodb";
+// import { getGroup } from "../../../lib/groups";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { id } = req.query; // dynamic id for group specific endpoint
+    const authToken = req?.headers?.authorization?.replace("Bearer ", "");
+
+    if (!id || !authToken) {
+      res.status(400).json({ message: "Missing parameters" });
+    }
+
+    switch (req.method) {
+      case "GET":
+        if (!id) return res.status(400).json({ message: "Missing body parameters" });
+
+        const client = await clientPromise;
+        const db = client.db("secret_santa");
+        const user = await db.collection("members").findOne({ _id: new ObjectId(id as string) });
+
+        if (!user) {
+          return res.status(400).json({ message: "No user found" });
+        }
+
+        if (user.account.token && user.account.token === authToken) {
+          return res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            subGroup: user.subGroup,
+            secret: user.secret,
+            account: { token: user.account.token },
+          });
+        }
+        return res.status(400).json({ message: "Auth refused" });
+
+        break;
+    }
+  } catch (e) {
+    res.status(400).json({ error: { message: "Error" } });
+  }
+};
