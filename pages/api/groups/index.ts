b132ff13d00bbb05c10b9c184696d62111574d92
Branda0 <g.brandao.dev@gmail.com>
2022-11-27 22:42:18 +0100

    api/groups fix => added second filter parameter (group + member name )

--- pages/api/groups/index.ts
+++ pages/api/groups/index.ts
@@ -10,42 +10,62 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         const client = await clientPromise;
         const db = client.db("secret_santa");
         const { name, members } = req.body;
-        console.log("okoko", members);
+
         const membersSanta = computeGraph(members);
+        const membersNumber = membersSanta.length;
 
         // if graph compute fails => too much constrains
         if (!membersSanta) {
           res.status(400).json({
             error: { message: "Distribution des cadeaux impossible ! Modifiez vos contraintes de groupe" },
           });
+          break;
         }
 
-        if (name && members) {
-          if (await db.collection("groups").findOne({ name: name })) {
-            res.status(409).json({ error: { message: "Nom de groupe déjà existant !" } });
-          }
+        // body parameters missing
+        if (!name || !members) {
+          res.status(400).json({ error: { message: "Erreur de création de groupe, veuillez réessayer" } });
+          break;
+        }
 
-          const membersResponse = await db.collection("members").insertMany(members);
-          const membersNumber = membersSanta.length;
+        // group name already in DB
+        if (await db.collection("groups").findOne({ name: name })) {
+          res.status(409).json({ error: { message: "Nom de groupe déjà existant !" } });
+          break;
+        }
 
-          for (let i = 0; i < membersNumber - 1; i++) {
-            await db
-              .collection("members")
-              .updateOne({ name: membersSanta[i] }, { $set: { secret: membersSanta[i + 1] } });
-          }
+        const groupResponse = await db.collection("groups").insertOne({ name, members: members });
+
+        const membersResponse = await db.collection("members").insertMany(members);
+        console.log(Object.values(membersResponse.insertedIds));
+        await db
+          .collection("members")
+          .updateMany(
+            { _id: { $in: Object.values(membersResponse.insertedIds) } },
+            { $set: { group: groupResponse.insertedId } }
+          );
+
+        for (let i = 0; i < membersNumber - 1; i++) {
           await db
             .collection("members")
-            .updateOne({ name: membersSanta[membersNumber - 1] }, { $set: { secret: membersSanta[0] } });
-
-          const groupResponse = await db.collection("groups").insertOne({ name, members });
-
-          res.status(200).json({
-            // data: await db.collection("groups").findOne({ _id: groupResponse.insertedId }),
-          });
-          break;
-        } else {
-          res.status(400).json({ error: { message: "Erreur de création de groupe, veuillez réessayer" } });
+            .updateOne(
+              { group: groupResponse.insertedId, name: membersSanta[i] },
+              { $set: { secret: membersSanta[i + 1] } }
+            );
         }
+        await db
+          .collection("members")
+          .updateOne(
+            { group: groupResponse.insertedId, name: membersSanta[membersNumber - 1] },
+            { $set: { secret: membersSanta[0] } }
+          );
+
+        await db.collection("groups").updateOne({ name: name }, { $set: { members } });
+
+        res.status(200).json({
+          data: await db.collection("groups").findOne({ _id: groupResponse.insertedId }),
+        });
+        break;
 
       case "GET":
         const allGroups = await getAllGroups();
