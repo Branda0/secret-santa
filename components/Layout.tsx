ba3208baa89c6833be98f0204854351f7b73ae1e
Branda0 <g.brandao.dev@gmail.com>
2022-11-27 22:31:28 +0100

    reworked Layout, style + context data / login

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,62 +1,55 @@
 import Image from "next/image";
-import { Roboto } from "@next/font/google";
-import { useState, useContext } from "react";
 
-import { UserContext } from "../context/User";
+import { useContext } from "react";
+import Cookies from "js-cookie";
 
-interface AppContextInterface {
-  isLogged?: boolean;
-  setIsLogged?: React.Dispatch<React.SetStateAction<boolean>>;
-  user?: { userId: string; userToken: string };
-  setUser?: React.Dispatch<React.SetStateAction<{ userId: string; userToken: string }>>;
-}
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
-const roboto = Roboto({
-  weight: ["300", "400", "700"],
-  style: ["normal", "italic"],
-  subsets: ["latin"],
-});
+import { UserContext, AppContextInterface } from "../context/User";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  // const isLogged = useContext(User);
-  // const setIsLogged = useContext(Config);
-  const { isLogged, setIsLogged } = useContext(UserContext) as AppContextInterface;
+  const { isLogged, updateName, updateToken } = useContext(UserContext) as AppContextInterface;
 
+  const handleLogout = () => {
+    // removes Cookies from browser [userId and auth token]
+    updateName(null);
+    updateToken(null);
+  };
   return (
-    <div className="">
-      <header className="flex flex-col  bg-[#F9ADA0]">
-        <div className="self-end">
-          {isLogged ? <span className="text-white ">{`Hello TOI`}</span> : null}
-          {/* <span className="text-white ">{`context name :${name}`}</span> */}
+    <div className="flex flex-col min-h-screen">
+      <header className="flex flex-col  bg-[#F9ADA0] p-2 pb-0 ">
+        {isLogged ? (
+          <div className=" flex items-center self-end absolute">
+            <FontAwesomeIcon icon={faUser} className="w-3 mr-2   text-white" />
+            <span className="text-white capitalize font-medium">{`Hello ${Cookies.get(
+              "santa-userId"
+            )}`}</span>
+            <button className="btn-red px-3 ml-4 text-xs font-medium" onClick={handleLogout}>
+              Se déconnecter
+            </button>
+          </div>
+        ) : null}
 
-          <button onClick={() => setIsLogged((prev: boolean) => !prev)}>deco</button>
-        </div>
         <div className="flex flex-col">
-          <div className="relative h-[8rem] w-full">
+          <div className="relative h-[8rem] w-full mt-10 md:mt-5">
             <Image
               fill
               src={"https://3foisrien.shop/wp-content/uploads/2021/11/Sans-titre-3.png"}
               alt="secret santa logo"
-              className="object-contain "
+              className="object-contain"
             />
           </div>
         </div>
-        <div className=" flex flex-col w-full px-3 ">
-          <span className="rounded-xl justify-self-center text-[3rem] text-center bg-red-500 text-white font-bold ">
+        <div className="self-center flex flex-col w-full px-3 md:w-[70rem]  ">
+          <span className="rounded-t-xl justify-self-center p-3 text-4xl md:text-5xl text-center bg-red-500 text-white font-bold ">
             Secret Santa
           </span>
         </div>
       </header>
 
-      <main className={`${roboto.className} flex flex-col self-center max-w-4xl m-auto my-3`}>
-        {children}
-      </main>
-      <footer className="flex justify-center w-full border-2 border-black ">
-        Image by{" "}
-        {/* <a href="https://www.freepik.com/free-vector/hand-drawn-flat-secret-santa-illustration_20112078.htm#page=2&query=secret%20santa&position=22&from_view=keyword">
-          Freepik
-        </a> */}
-      </footer>
+      <main className={" flex-2 flex-col self-center max-w-4xl px-3 sm:px-10 m-auto my-3 "}>{children}</main>
+      <footer className="mt-auto w-full "></footer>
     </div>
   );
 };
