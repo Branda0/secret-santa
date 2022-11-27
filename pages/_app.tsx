d5218270fe2d09548147e29fb78f3c5f4e369f62
Branda0 <g.brandao.dev@gmail.com>
2022-11-27 22:33:58 +0100

    removed Layout usage from App root

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,20 @@
 import "../styles/globals.css";
 import UserProvider from "../context/User";
 import type { AppProps } from "next/app";
+import { Montserrat } from "@next/font/google";
 
-import Layout from "../components/Layout";
+const montserrat = Montserrat({
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider>
-      <Layout>
+    <div className={montserrat.className}>
+      <UserProvider>
         <Component {...pageProps} />
-      </Layout>
-    </UserProvider>
+      </UserProvider>
+    </div>
   );
 }
